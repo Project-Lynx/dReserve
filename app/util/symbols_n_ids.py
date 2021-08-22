d8f890f479e477dd9c1da5ec387b8fdc9bf370f3
Antonio Hickey <antoniohickey99@gmail.com>
2021-08-22 08:31:41 -0400

    Cleaned by linter

--- app/util/symbols_n_ids.py
+++ app/util/symbols_n_ids.py
@@ -3,6 +3,8 @@ from app.data.CME_Futures import futures_map
 """
     Convert Futures symbols to product id's
 """
+
+
 def get_CME_pids(symbols):
     if isinstance(symbols, str):
         return futures_map[symbols]
@@ -13,4 +15,3 @@ def get_CME_pids(symbols):
             pids.append(futures_map[symbol])
 
         return pids
-
