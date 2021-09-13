0442ede3d386f14384371ce97bcade584e8e1cb9
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-12 23:48:19 -0400

    added Type Hints

--- app/util/symbols_n_ids.py
+++ app/util/symbols_n_ids.py
@@ -1,11 +1,10 @@
+from typing import Union
+
 from app.data.CME_Futures import futures_map
 
-"""
-    Convert Futures symbols to product id's
-"""
 
-
-def get_CME_pids(symbols):
+# Convert Futures symbols to product id's
+def get_CME_pids(symbols: Union[str, list]) -> Union[str, list]:
     if isinstance(symbols, str):
         return futures_map[symbols]
 
