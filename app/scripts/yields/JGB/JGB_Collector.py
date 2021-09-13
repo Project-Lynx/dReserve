fd7b10d5e7ee77948c7709b4fc59bccdd6fb0498
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-13 03:02:22 -0400

    Updated data collection method

--- app/scripts/yields/JGB/JGB_Collector.py
+++ app/scripts/yields/JGB/JGB_Collector.py
@@ -1,45 +1,40 @@
-import urllib.request
+import requests as req
+from bs4 import BeautifulSoup as bs
 
 from app.models.yields import Meta as yields_model
-from app.util.links import JGB_LINK
+from app.util import dates
 
 model = yields_model()
-data = urllib.request.urlopen(JGB_LINK)
+
+url = "http://www.worldgovernmentbonds.com/bond-historical-data/japan/1-month/"
+response = req.request("GET", url)
+parsed = bs(response.text, "lxml")
+
+target_data = parsed.findAll("tbody")[-1]
+date_item = parsed.find("p", {'class': "w3-small w3-right"}).text
+date_pre = list(date_item.split(" "))
+day = date_pre[2]
+month_pre = date_pre[3]
+month = dates.convert_shorthand_month(month_pre)
+year = date_pre[4]
+date_final = f"{year}-{month}-{day}"
+
 count = 0
+_temp_list_ = [date_final]
 
-for line in data.readlines():
-    count += 1
-    if count > 2:
-        start = str(line).find("'") + 1
-        end = str(line).find("\\")
-        line = str(line)[start:end]
+for i in target_data:
+    if hasattr(i, 'bs4.element.Tag'):
+        if "%" in i.text:
+            count += 1
+            if count not in [4, 8, 10, 12, 13]:
+                d_list = (i.text).splitlines()
+                for j in d_list:
+                    if "%" in j:
+                        _temp_list_.append(j)
+temp_list = [tuple(_temp_list_)]
+vals_ph = "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-        list_ = list(line.split(","))
-
-        # Converting YYYY/m/d -> YYYY-MM-DD
-        _date = (list_[0])
-        y_start = 0
-        y_end = _date.find("/")
-        year = _date[y_start:y_end]
-        m_start = _date.find("/") + 1
-        m_end = _date[m_start:].find("/")+(m_start)
-        month = _date[m_start:m_end]
-        if len(month) == 1:
-            month = f"0{month}"
-        d_start = _date[m_start:].find("/")+(m_start+1)
-        day = _date[d_start:]
-        if len(day) == 1:
-            day = f"0{day}"
-        _date = f"{year}-{month}-{day}"
-
-        temp_list = [(_date, list_[1], list_[2],
-                     list_[3], list_[4], list_[5],
-                     list_[6], list_[7], list_[8],
-                     list_[9], list_[10], list_[11],
-                     list_[12], list_[13], list_[14],
-                     list_[15])]
-
-        model.executemany(
-            "INSERT INTO JGB_table VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-            temp_list,
-        )
+model.executemany(
+    f"INSERT INTO JGB_table VALUES {vals_ph}",
+    temp_list,
+)
