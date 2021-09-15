fd8ac9f1c3947e8b32cd8b27bd5a22c46b5a69dc
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-15 02:07:37 -0400

    Added Cross Origin Resource Sharing

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from app.routes import events, futures, yields
 
 application = Flask(__name__)
 app = application
+CORS(app)
 
 app.register_blueprint(futures.blueprint)
 app.register_blueprint(events.blueprint)
