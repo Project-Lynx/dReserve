924c48da4ffefea4e033f7b6ef4a96e303721ce6
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-20 02:55:53 -0400

    Cleaning up code

--- app/scripts/yields/JGB/JGB_Collector.py
+++ app/scripts/yields/JGB/JGB_Collector.py
@@ -2,26 +2,30 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 
 from app.models.yields import Meta as yields_model
-from app.util import dates
+from app.util import dates, tables
 
+# Define model & table
 model = yields_model()
+table = tables.get_table("JGB")
 
+# Collect data
 url = "http://www.worldgovernmentbonds.com/bond-historical-data/japan/1-month/"
 response = req.request("GET", url)
 parsed = bs(response.text, "lxml")
 
+# Parse dates to target format
 target_data = parsed.findAll("tbody")[-1]
 date_item = parsed.find("p", {'class': "w3-small w3-right"}).text
 date_pre = list(date_item.split(" "))
 day = date_pre[2]
-month_pre = date_pre[3]
-month = dates.convert_shorthand_month(month_pre)
+month = dates.convert_shorthand_month(date_pre[3])
 year = date_pre[4]
+
 date_final = f"{year}-{month}-{day}"
 
+# Parse data to fit model
+_list_: list = []
 count = 0
-_temp_list_ = [date_final]
-
 for i in target_data:
     if hasattr(i, 'bs4.element.Tag'):
         if "%" in i.text:
@@ -30,11 +34,15 @@ for i in target_data:
                 d_list = (i.text).splitlines()
                 for j in d_list:
                     if "%" in j:
-                        _temp_list_.append(j)
-temp_list = [tuple(_temp_list_)]
-vals_ph = "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                        _list_.append(j)
+_list_.append(date_final)
+_list_.append(year)
+temp_list = [tuple(_list_)]
 
+# Adding data to table
+cols = "(m1, m3, m6, y1, y2, y3, y5, y7, y10, y15, y20, y30, y40, date, year)"
+vals_ph = "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 model.executemany(
-    f"INSERT INTO JGB_table VALUES {vals_ph}",
+    f"INSERT INTO {table} {cols} VALUES {vals_ph}",
     temp_list,
 )
--- app/scripts/yields/ukGB/ukGB_Collector.py
+++ app/scripts/yields/ukGB/ukGB_Collector.py
@@ -2,42 +2,46 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 
 from app.models.yields import Meta as yields_model
-from app.util import dates
+from app.util import dates, tables
 
+# Define model & table
 model = yields_model()
+table = tables.get_table("UKGB")
 
+# Collect data
 url = "http://www.worldgovernmentbonds.com/bond-historical-data/united-kingdom/1-month/"
 response = req.request("GET", url)
 parsed = bs(response.text, "lxml")
 
+# Parse dates to target format
 data = parsed.findAll("td", {'class': "w3-center"})
 date = parsed.find("p", {'class': "w3-small w3-right"}).text
-d_start = date.find(":")+2
-d_end = date.find("G")-7
-date = date[d_start:d_end]
-date_list = list(date.split(" "))
-day = date_list[0]
-month = date_list[1]
-month = dates.convert_shorthand_month(month)
-year = date_list[2]
+date_list = list(date.split())
+day = date_list[2]
+month = dates.convert_shorthand_month(date_list[3])
+year = date_list[4]
 
-date = f"{year}-{month}-{day}"
+date_final = f"{year}-{month}-{day}"
 
+# Define collected data
 target_data = data[-80:]
 
-
-_list_ = [date]
+# Parse data to fit model
+_list_: list = []
 count = 0
 for i in target_data:
     if "%" in i.text:
         count += 1
         if count not in [7, 9, 11, 12, 14, 20]:
             _list_.append(i.text[:-1])
+_list_.append(date_final)
+_list_.append(year)
 temp_list = [tuple(_list_)]
 
-vals_ph = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s"
-
+# Adding data to table
+cols = "(m1, m3, m6, y1, y2, y3, y5, y7, y10, y15, y20, y25, y30, y40, date, year)"
+vals_ph = "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 model.executemany(
-    f"INSERT INTO UKGB_table VALUES ({vals_ph})",
+    f"INSERT INTO {table} {cols} VALUES {vals_ph}",
     temp_list,
 )
