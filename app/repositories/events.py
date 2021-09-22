0a4872a4ab8b0bc7bc3b0ab663fce71af973a82b
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-22 03:20:02 -0400

    Minor cleaning

--- app/repositories/events.py
+++ app/repositories/events.py
@@ -1,8 +1,6 @@
 from app.models.events import Meta
 
 
-def get_events(regions: str) -> dict:
+def get_events(regions: list) -> dict:
     events_model = Meta()
-    output = events_model.to_dict(regions)
-
-    return output
+    return events_model.to_dict(regions)
