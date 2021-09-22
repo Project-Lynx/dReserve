23191371fa82243320b2d780f39df5c24d871a74
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-22 03:38:05 -0400

    Added logic + cleaning code

--- app/repositories/events.py
+++ app/repositories/events.py
@@ -1,6 +1,7 @@
 from app.models.events import Meta
 
 
-def get_events(regions: list) -> dict:
+def get_events(regions: str) -> dict:
+    regions_list = list(regions.split(","))
     events_model = Meta()
-    return events_model.to_dict(regions)
+    return events_model.to_dict(regions_list)
