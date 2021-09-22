6c06ad233abd27e789f983cc5023edbdf60068eb
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-22 03:35:11 -0400

    Nuked JGB_LINK (Useless)

--- app/util/links.py
+++ app/util/links.py
@@ -1,3 +1,2 @@
-JGB_LINK = "https://www.mof.go.jp/english/policy/jgbs/reference/interest_rate/jgbcme.csv"
 CME_API_BASE = "https://www.cmegroup.com/CmeWS/mvc/Quotes/ContractsByNumber?productIds="
 CME_API_END = "&contractsNumber=1&venue=G&type=VOLUME"
