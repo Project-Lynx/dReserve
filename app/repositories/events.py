d8ba4fefa5bb6c207ded0f6332db5bb17a7a5957
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-23 18:25:40 -0400

    Refactored into smaller modules

--- app/repositories/events.py
+++
@@ -1,7 +0,0 @@
-from app.models.events import Meta
-
-
-def get_events(regions: str) -> dict:
-    regions_list = list(regions.split(","))
-    events_model = Meta()
-    return events_model.to_dict(regions_list)
