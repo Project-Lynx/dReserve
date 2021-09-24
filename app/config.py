26ec2b8de94bf338480c5879311f26a03370fdae
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-23 21:42:03 -0400

    Added read only user

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
-RDS_HOST = " *REPLACE WITH YOUR RDS HOSTNAME* "
+"""
+    Read only user, if you're forking this then
+    change this with the admin user/password
+    and host/port to your own MySQL database.
+"""
+RDS_HOST = "dreserve.cqcxzsq9ywqh.us-east-2.rds.amazonaws.com"
 RDS_PORT = 3306
-RDS_USER = ' *REPLACE WITH YOUR RDS USERNAME* '
-RDS_PWORD = ' *REPLACE WITH YOUR RDS PASSWORD* '
+RDS_USER = 'readonly'
+RDS_PWORD = '%i+5R3aD0lvLy$'
 
 WEB_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
 WEB_BINARY_PATH = "/usr/bin/google-chrome-stable"
