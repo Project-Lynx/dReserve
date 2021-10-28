47f98dbb40dffbb74572c22e961cb4d43e6dafe6
Antonio Hickey <antoniohickey99@gmail.com>
2021-10-28 16:49:03 -0400

    fix(database_meta): docstring styling

--- app/models/database_meta.py
+++ app/models/database_meta.py
@@ -4,8 +4,7 @@ from app.config import RDS_HOST, RDS_PORT, RDS_PWORD, RDS_USER
 
 
 class DB_Meta():
-
-    """Model for interacting with database"""
+    """Model for interacting with database."""
     def __init__(self, database: str) -> None:
         """Set up database variables."""
         self.host = RDS_HOST
