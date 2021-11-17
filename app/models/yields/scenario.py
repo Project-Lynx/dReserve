d2c2337130d350f673da7ce1f99ed25aeb93a73c
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-17 05:16:13 -0500

    refactor: for new data source

--- app/models/yields/scenario.py
+++ app/models/yields/scenario.py
@@ -1,50 +1,56 @@
 from typing import Union
 
-from app.models.yields.product import Product
+from app.util.convert_product import get_class
 
 
 class No_Dates:
     """Strategy Interface for when no dates are passed."""
-    def execute(self, product: str, duration: str = None) -> None:
+    def execute(self, product: str, duration: str = "") -> None:
         """Execute no dates strategy."""
-        _Product_ = Product(product, duration=duration)
-        product_data = _Product_.export_info()
-        table = product_data[0]
-        columns = product_data[1]
-        strategy = product_data[2]
+        product_class = get_class(product)
+        product_data = product_class().export_info()
+        table = product_data['table']
+        columns = product_data['columns']
 
         if duration:
             query = [f"SELECT date,{duration} FROM {table}", False]
-            _Product_.rate_to_dict(query)
+            product_class().rate_to_dict(query)
         else:
             query = [f"SELECT {columns} FROM {table}", False]
-            strategy().to_dict(query)
+            product_class().to_dict(query)
 
 
-class Dates_Strategy(Product):
+class Dates_Strategy:
     """Strategy Interface for when dates are passed."""
-    def __init__(self, product: str, dates: list, duration: str = None) -> None:
+    def __init__(self, product: str, dates: list, duration: str = "") -> None:
         self.dates = dates
         self.duration = duration
+        self.product = product
         self.current_query_end = "ORDER BY id DESC LIMIT 1"
         self.previous_query_end = "ORDER BY id DESC LIMIT 1, 1"
-        if duration:
-            super().__init__(product, data=dates, duration=duration)
-            self.query_base = f"SELECT date,{duration} FROM {self.table} "
-        else:
-            super().__init__(product, data=dates)
-            self.query_base = f"SELECT {self.columns} FROM {self.table} "
+        self.product_class = get_class(product)
 
-    def get_internals(self) -> list:
+        internals = self.product_class().export_info()
+
+        if duration:
+            self.query_base = f"SELECT date,{duration} FROM {internals['table']} "
+        else:
+            self.query_base = f"SELECT {internals['columns']} FROM {internals['table']} "
+
+    def get_internals(self) -> dict:
         """Export internal variables."""
-        return [self.product, self.dates, self.duration]
+        return {
+            'product': self.product,
+            'dates': self.dates,
+            'duration': self.duration,
+        }
 
     def execute(self, query: list) -> dict:
         """Execute Strategy."""
         if self.duration:
-            return self.rate_to_dict(query)
+            return self.product_class().rate_to_dict(query)
         else:
-            return self.product_strategy(self.dates).to_dict(query)
+            return self.product_class(self.dates).to_dict(query)
 
 
 class a_date(Dates_Strategy):
@@ -97,11 +103,13 @@ class Context:
     def execute_strategy(self, strategy: Dates_Strategy) -> dict:
         """Execute strategy for a specific scenario."""
         internals = strategy.get_internals()
-        product = internals[0]
-        dates = internals[1]
-        duration = internals[2]
 
-        if type(strategy) == a_date:
-            return strategy.execute(a_date(product, dates, duration).make_query())
+        if isinstance(strategy, a_date):
+            return strategy.execute(a_date(
+                internals['product'], internals['dates'], internals['duration'],
+            ).make_query())
+
         else:
-            return strategy.execute(multi_dates(product, dates, duration).make_query())
+            return strategy.execute(multi_dates(
+                internals['product'], internals['dates'], internals['duration'],
+            ).make_query())
