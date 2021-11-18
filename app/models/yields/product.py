ceddf623a0bbbe2c296cf772c717c35940bc2d43
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-17 23:43:45 -0500

    feat: add model for egyptian yields

--- app/models/yields/product.py
+++ app/models/yields/product.py
@@ -462,3 +462,31 @@ class Czech_Republic(Product):
                     50y varchar(7), date DATE, year YEAR)
                 """
         Yields_DB().create_table(query)
+
+
+class Egypt(Product):
+    """Egyptian Government Bonds."""
+    def __init__(self, data: list = []) -> None:
+        super().__init__(product="CzechRepublicGB", data=data)
+        self.columns = "1y,2y,3y,4y,5y,6y,7y,8y,9y,10y,15y,20y,50y,date"
+
+    def to_dict(self, query: list = []) -> dict:
+        for idx in enumerate(self.fetch_data(query)):
+            key = str(idx[1][-2])
+            self.output[key] = {
+                'Overnight': idx[1][0], '3 Month': idx[1][1], '6 Month': idx[1][2],
+                '9 Month': idx[1][3], '1 Year': idx[1][4], '2 Year': idx[1][5],
+                '3 Year': idx[1][6], '5 Year': idx[1][7], '7 Year': idx[1][8],
+                '10 Year': idx[1][9],
+            }
+        return self.output
+
+    def create_table(self) -> None:
+        query = """CREATE TABLE IF NOT EXISTS CzechRepublicGB
+                   (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    on varchar(7), 3m varchar(7), 6m varchar(7),
+                    9m varchar(7), 1y varchar(7), 2y varchar(7),
+                    3y varchar(7), 5y varchar(7), 7y varchar(7),
+                    10y varchar(7), date DATE, year YEAR)
+                """
+        Yields_DB().create_table(query)
