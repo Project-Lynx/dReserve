9887242231d9c1ef0e0d5f80bdbb4ff0555b8eb8
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-18 00:14:40 -0500

    feat: add french yields model

--- app/models/yields/product.py
+++ app/models/yields/product.py
@@ -490,3 +490,37 @@ class Egypt(Product):
                     10y varchar(7), date DATE, year YEAR)
                 """
         Yields_DB().create_table(query)
+
+
+class France(Product):
+    """French Government Bonds."""
+    def __init__(self, data: list = []) -> None:
+        super().__init__(product="FranceGB", data=data)
+        self.columns = "1y,2y,3y,4y,5y,6y,7y,8y,9y,10y,15y,20y,50y,date"
+
+    def to_dict(self, query: list = []) -> dict:
+        for idx in enumerate(self.fetch_data(query)):
+            key = str(idx[1][-2])
+            self.output[key] = {
+                '1 Month': idx[1][0], '3 Month': idx[1][1], '6 Month': idx[1][2],
+                '9 Month': idx[1][3], '1 Year': idx[1][4], '2 Year': idx[1][5],
+                '3 Year': idx[1][6], '4 Year': idx[1][7], '5 Year': idx[1][8],
+                '6 Year': idx[1][9], '7 Year': idx[1][10], '8 Year': idx[1][11],
+                '9 Year': idx[1][12], '10 Year': idx[1][13], '15 Year': idx[1][14],
+                '20 Year': idx[1][15], '25 Year': idx[1][16], '30 Year': idx[1][17],
+                '50 Year': idx[1][18],
+            }
+        return self.output
+
+    def create_table(self) -> None:
+        query = """CREATE TABLE IF NOT EXISTS FranceGB
+                   (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    1m varchar(7), 3m varchar(7), 6m varchar(7),
+                    9m varchar(7), 1y varchar(7), 2y varchar(7),
+                    3y varchar(7), 4y varchar(7), 5y varchar(7),
+                    6y varchar(7), 7y varchar(7), 8y varchar(7),
+                    9y varchar(7), 10y varchar(7), 15y varchar(7),
+                    20y varchar(7), 25y varchar(7), 30 varchar(7),
+                    50y varchar(7), date DATE, year YEAR)
+                """
+        Yields_DB().create_table(query)
