1b617090cb4da5d77b1c996648b1db22d7df47af
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-18 00:24:39 -0500

    feat: add german yields model

--- app/models/yields/product.py
+++ app/models/yields/product.py
@@ -524,3 +524,35 @@ class France(Product):
                     50y varchar(7), date DATE, year YEAR)
                 """
         Yields_DB().create_table(query)
+
+
+class Germany(Product):
+    """German Government Bonds."""
+    def __init__(self, data: list = []) -> None:
+        super().__init__(product="FranceGB", data=data)
+        self.columns = "3m,6m,9m,1y,2y,3y,4y,5y,6y,7y,8y,9y,10y,15y,20y,25y,30y,date"
+
+    def to_dict(self, query: list = []) -> dict:
+        for idx in enumerate(self.fetch_data(query)):
+            key = str(idx[1][-2])
+            self.output[key] = {
+                '3 Month': idx[1][0], '6 Month': idx[1][1], '9 Month': idx[1][2],
+                '1 Year': idx[1][3], '2 Year': idx[1][4], '3 Year': idx[1][5],
+                '4 Year': idx[1][6], '5 Year': idx[1][7], '6 Year': idx[1][8],
+                '7 Year': idx[1][9], '8 Year': idx[1][10], '9 Year': idx[1][11],
+                '10 Year': idx[1][12], '15 Year': idx[1][13], '20 Year': idx[1][14],
+                '25 Year': idx[1][15], '30 Year': idx[1][16],
+            }
+        return self.output
+
+    def create_table(self) -> None:
+        query = """CREATE TABLE IF NOT EXISTS FranceGB
+                   (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    3m varchar(7), 6m varchar(7), 9m varchar(7),
+                    1y varchar(7), 2y varchar(7), 3y varchar(7),
+                    4y varchar(7), 5y varchar(7), 6y varchar(7),
+                    7y varchar(7), 8y varchar(7), 9y varchar(7),
+                    10y varchar(7), 15y varchar(7), 20y varchar(7),
+                    25y varchar(7), 30y varchar(7), date DATE, year YEAR)
+                """
+        Yields_DB().create_table(query)
