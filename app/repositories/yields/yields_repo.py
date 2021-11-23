286578d60a37aeb00b9cefb3bb22a02544b3ec78
Antonio Hickey <antoniohickey99@gmail.com>
2021-11-23 04:21:00 -0500

    fix: no-self-use (R0201)

--- app/repositories/yields/yields_repo.py
+++ app/repositories/yields/yields_repo.py
@@ -4,16 +4,18 @@ from app.models.yields.scenario import Context, No_Dates, a_date, multi_dates
 
 
 class Yields_Repo:
-    def get_data(self, payload: list) -> Union[dict, None]:
-        """Run logic to execute the correct scenario strategy."""
-        product = payload[0]
-        dates_str = payload[1]
-        duration = payload[2]
-        if dates_str is None:
-            return No_Dates().execute(product, duration)
+    def __init__(self, payload: dict) -> None:
+        self.product = payload['product']
+        self.dates_str = payload['dates']
+        self.duration = payload['duration']
 
-        dates_list = dates_str.split(",")
+    def get_data(self) -> Union[dict, None]:
+        """Run logic to execute the correct scenario strategy."""
+        if self.dates_str is None:
+            return No_Dates().execute(self.product, self.duration)
+
+        dates_list = self.dates_str.split(",")
         if len(dates_list) > 1:
-            return Context().execute_strategy(multi_dates(product, dates_list, duration))
+            return Context().execute_strategy(multi_dates(self.product, dates_list, self.duration))
         else:
-            return Context().execute_strategy(a_date(product, dates_list, duration))
+            return Context().execute_strategy(a_date(self.product, dates_list, self.duration))
